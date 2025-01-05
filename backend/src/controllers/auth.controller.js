95b095fccba5b1bc2ee6f29b7220a5a6be212274
Jakub Pawiński <j.pawinski.164@studms.ug.edu.pl>
2025-01-05 12:57:49 +0100

    Auth endpoint

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,33 +1,57 @@
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcryptjs';
 import User from '../models/user.model.js';
 import { findUserByEmail } from '../services/user.service.js';
+
+const saltRounds = 10;
 
 const login = async (req, res) => {
 	const { email, password } = req.body;
 	try {
-		const user = findUserByEmail(email);
+		const user = await findUserByEmail(email);
 		if (!user) {
 			return res.status(404).json({ message: 'User not found' });
 		}
-		res.status(200).json({ message: 'User logged in successfully' });
+
+		const isPasswordCorrect = await bcrypt.compare(password, user.password);
+		if (!isPasswordCorrect) {
+			return res.status(400).json({ message: 'Invalid password' });
+		} else {
+			const token = jwt.sign(
+				{ email: user.email, id: user._id, role: user.role },
+				process.env.JWT_Secret_Key,
+				{
+					expiresIn: '24h',
+				}
+			);
+			res.cookie('token', token, {
+				maxAge: 24 * 60 * 60 * 1000,
+			});
+
+			return res.status(200).json({ message: 'User logged in successfully' });
+		}
 	} catch (error) {
-		res.status(404).json({ message: error.message });
+		res.status(500).json({ message: error.message });
 	}
 };
 const register = async (req, res) => {
-	const { firstName, lastName, email, password, phone, role } = req.body;
 	try {
-		if (await findUserByEmail(email)) {
+		const { firstName, lastName, email, password, phone, role } = req.body;
+
+		if (!(await findUserByEmail(email))) {
 			return res.status(400).json({ message: 'User already exists' });
 		}
+		const hashedPassword = await bcrypt.hash(password, saltRounds);
+
 		const user = await User.create({
 			firstName,
 			lastName,
 			email,
-			password,
+			hashedPassword,
 			phone,
 			role,
 		});
-		res.status(201).json(user);
+		res.status(201).json({ message: 'Acount created', user: { ...user } });
 	} catch (error) {
 		res.status(404).json({ message: error.message });
 	}
